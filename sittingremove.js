95a0b626e3db635f627b6cde55b5154d96fffa5d
md.ashrafulislam <ksashraful94@gmail.com>
2020-10-22 10:00:19 +0600

    first commit

---
+++ sittingremove.js
@@ -0,0 +1,24 @@
+//sittings && remove object
+
+//sitting object
+// let point = {
+//     a : 10,
+//     b : 20, 
+//     c : 30
+// }
+// point.d = 40
+// console.log(point)
+
+//remove object
+// point.a = null
+// delete point.a     //delete operator
+// console.log(point)
+// let name='ashraf'
+// let r= 30
+// let b= 35
+// let s= (r*100)/35
+// console.log(name  + ': strike rate ' + s)
+
+function name(params) {
+    
+}
